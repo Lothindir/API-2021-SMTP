632e9d2c53a1a0dd40583e3432bded92c26ac046
Anthony Coke <anthony.coke@heig-vd.ch>
2021-11-24 20:40:28 +0100

    refactor: add parse method in constructor

--- src/main/java/ch/heigvd/mail/Mail.java
+++ src/main/java/ch/heigvd/mail/Mail.java
@@ -1,22 +1,34 @@
 package ch.heigvd.mail;
 
-import ch.heigvd.prank.Prank;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+@Getter @Setter
 public class Mail {
-    private final Person sender;
-    private final Prank prank;
+    private String from;
+    private List<String> to;
+    private List<String> cc;
+    private String subject;
+    private String body;
 
-    public Mail(Person sender, Prank prank) {
-        this.sender = sender;
-        this.prank = prank;
+    public Mail(String message) {
+        parseMessage(message);
     }
 
-    public Prank getPrank() {
-        return prank;
+    private void parseMessage(String message) {
+        String[] strs = message.split("\n");
+        if(strs[0].contains("Subject:")) {
+            subject = strs[0];
+        }
+        StringBuilder sb = new StringBuilder();
+        for(int i = 1; i < strs.length; ++i) {
+            sb.append(strs[i]).append("\n");
+        }
+        body = sb.toString();
     }
-
-    public Person getSender() {
-        return sender;
-    }
-
 }
