f36e12a6e8a00351f72540c30d302cdd90c6b1ec
Anthony Coke <anthony.coke@heig-vd.ch>
2021-11-24 20:39:34 +0100

    refactor: change class content, add methods

--- src/main/java/ch/heigvd/mail/Group.java
+++ src/main/java/ch/heigvd/mail/Group.java
@@ -1,20 +1,21 @@
 package ch.heigvd.mail;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Group {
     ArrayList<Person> people = new ArrayList<>();
 
-    /**
-     *
-     * @param person
-     */
     public void addPerson(Person person) {
         people.add(person);
     }
 
     public ArrayList<Person> getPeople(){
         return new ArrayList<>(people);
+    }
+
+    public void setPeople(List<Person> newPeople) {
+        people = new ArrayList<>(newPeople);
     }
 
 }
