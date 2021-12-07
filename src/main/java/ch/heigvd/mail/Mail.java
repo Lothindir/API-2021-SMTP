50d6ad40389eb7940607ce9fc7b1bd57caca5282
Lothindir <lothindir@gmail.com>
2021-12-07 14:41:59 +0100

    refactor: Refactored and formatted Mail and Person

--- src/main/java/ch/heigvd/mail/Mail.java
+++ src/main/java/ch/heigvd/mail/Mail.java
@@ -1,10 +1,10 @@
 package ch.heigvd.mail;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
-
-import java.util.List;
 
 /**
  * Implementation of a Mail. It will run the prank campaign.
@@ -12,36 +12,56 @@ import java.util.List;
  * @author Anthony Coke
  * @author Francesco Monti
  */
-@Accessors(prefix = {"_"})
-@Getter @Setter
+@Getter
 public class Mail {
-    private String _from;
-    private List<String> _to;
-    private List<String> _cc;
-    private String _subject;
-    private String _body;
+   @Setter
+   private String from;
+   private List<String> to;
+   private List<String> cc;
+   private String subject;
+   private String body;
 
-    /**
-     * Constructor with parameters, it calls the parseMessage.
-     * @param message the message with its subject and body.
-     */
-    public Mail(String message) {
-        parseMessage(message);
-    }
+   /**
+    * Constructor with parameters, it calls the parseMessage.
+    * 
+    * @param message the message with its subject and body.
+    */
+   public Mail(String message) {
+      parseMessage(message);
+      to = new ArrayList<>();
+      cc = new ArrayList<>();
+   }
 
-    /**
-     * Retrieves the subject and the body of the message to be sent.
-     * @param message
-     */
-    private void parseMessage(String message) {
-        String[] strs = message.split("\n");
-        if(strs[0].contains("Subject:")) {
-            _subject = strs[0];
-        }
-        StringBuilder sb = new StringBuilder();
-        for(int i = 1; i < strs.length; ++i) {
-            sb.append(strs[i]).append("\n");
-        }
-        _body = sb.toString();
-    }
+   /**
+    * Retrieves the subject and the body of the message to be sent.
+    * 
+    * @param message
+    */
+   private void parseMessage(String message) {
+      String[] strs = message.split("\n");
+      if (strs[0].contains("Subject:")) {
+         subject = strs[0].replaceFirst("Subject:", "");
+      }
+      StringBuilder sb = new StringBuilder();
+      for (int i = 1; i < strs.length; ++i) {
+         sb.append(strs[i]).append("\n");
+      }
+      body = sb.toString();
+   }
+
+   public List<String> getTo() {
+      return new ArrayList<>(to);
+   }
+
+   public List<String> getCc() {
+      return new ArrayList<>(cc);
+   }
+
+   public void addTo(Person p) {
+      to.add(p.getEmailAddress());
+   }
+
+   public void addCc(Person p) {
+      cc.add(p.getEmailAddress());
+   }
 }
--- src/main/java/ch/heigvd/mail/Person.java
+++ src/main/java/ch/heigvd/mail/Person.java
@@ -1,26 +1,29 @@
 package ch.heigvd.mail;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 
 /**
- * Implementation of a person. It just stores an e-mail address retrieved from the victims.utf8 file.
+ * Implementation of a person. It just stores an e-mail address retrieved from
+ * the victims.utf8 file.
  *
  * @author Anthony Coke
  * @author Francesco Monti
  */
-@Accessors(prefix = {"_"})
-@Getter @Setter
+@Getter
+@Setter
+@RequiredArgsConstructor
 public class Person {
-    private final String _emailAddress;
+   private final String emailAddress;
 
-    /**
-     * Constructor with parameters.
-     * @param emailAddress the email address of the person to be created.
-     */
-    public Person(String emailAddress) {
-        _emailAddress = emailAddress;
-    }
+   /**
+    * Copy constructor
+
+    * @param other other person to copy from
+    */
+   public Person(Person other) {
+      this.emailAddress = other.emailAddress;
+   }
 
 }
